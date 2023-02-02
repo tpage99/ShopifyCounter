6ec29e04ac8af0a8118631f2232d98a9880e21ba
Taylor Page <taylorpage@Taylors-MBP-67.lan>
2023-02-01 22:14:45 -0500

    issue with product for node version then issue with local and just trying to use variables - oi

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -1,6 +1,7 @@
 exports.handler = async function() {
   const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${process.env.CHANNEL_ID}&key=${process.env.YT_API_KEY}`)
   const data = await response.json()
+  console.log(data)
   const ytItems = data.items
   const viewCount = ytItems[0].statistics.viewCount
   const ytURL = {
