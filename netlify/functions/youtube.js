7a407465f5b7d79e41f20bd659850c978fc57390
Taylor Page <taylorpage@Taylors-MBP-67.lan>
2023-02-02 06:37:57 -0500

    so close after refactor maybe

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -1,23 +1,32 @@
 import fetch from 'node-fetch'
-exports.handler = async function() {
-  const channel_Id = process.env.CHANNEL_ID
-  const Yt_Api_Key = process.env.YT_API_KEY
 
-  const YT_API = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channel_Id}&key=${Yt_Api_Key}`
-  const response = await fetch(YT_API)
-  const data = await response.json()
-  const ytItems = data.items
-  const viewCount = ytItems?.[0]?.statistics.viewCount
-  console.log(viewCount)
-  const ytURL = {
-    "number": `${viewCount}`
-  }
+const channel_Id = process.env.CHANNEL_ID
+const Yt_Api_Key = process.env.YT_API_KEY
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(ytURL),
-    headers: {
-			'Content-Type': 'application/json'
-		},
+const YT_API = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=` + channel_Id + `&key=` + Yt_Api_Key
+
+exports.handler = async (event, context) => {
+  try {
+    const response = await fetch(YT_API)
+    const data = await response.json()
+    const ytItems = data.items
+    const viewCount = ytItems?.[0]?.statistics.viewCount
+    console.log("this is the view count" + viewCount)
+    const ytURL = {
+      "number": viewCount
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(ytURL),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }  
+  } catch(error){
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Well that did NOT work'})
+    }
   }
 }
