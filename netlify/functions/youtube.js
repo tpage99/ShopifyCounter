01682e3dd7811335fbd08fa0ac1b18872d2335bb
Taylor Page <taylorpage@Taylors-MBP-67.lan>
2023-02-01 22:38:23 -0500

    updated for environment variables option and will double check settings

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -1,7 +1,10 @@
 import fetch from 'node-fetch'
 
 exports.handler = async function() {
-  const YT_API = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${process.env.CHANNEL_ID}&key=${process.env.YT_API_KEY}`
+  const channel_Id = process.env.CHANNEL_ID
+  const Yt_Api_Key = process.env.YT_API_KEY
+
+  const YT_API = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channel_Id}&key=${Yt_Api_Key}`
 
   const response = await fetch(YT_API)
   const data = await response.json()
