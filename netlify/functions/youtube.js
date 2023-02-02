c18d071079f5c8ad378c8ee724d81c83aa560cf1
Taylor Page <taylorpage@Taylors-MBP-67.lan>
2023-02-01 21:38:13 -0500

    keep getting a fetch is not defined error but works on my machine 🙄

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -1,7 +1,5 @@
 exports.handler = async function() {
-  const YT_API = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${process.env.CHANNEL_ID}&key=${process.env.YT_API_KEY}`
-
-  const response = await fetch(YT_API)
+  const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${process.env.CHANNEL_ID}&key=${process.env.YT_API_KEY}`)
   const data = await response.json()
   const ytItems = data.items
   const viewCount = ytItems[0].statistics.viewCount
