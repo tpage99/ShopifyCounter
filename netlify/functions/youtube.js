246519f12a9a972e0d541c821a9f8e3b7ac20517
Taylor Page <taylorpage@Taylors-MBP-67.lan>
2023-02-01 22:32:30 -0500

    works without fetch in dev but not in prod - crashes in dev with fetch

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -1,7 +1,10 @@
+import fetch from 'node-fetch'
+
 exports.handler = async function() {
-  const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${process.env.CHANNEL_ID}&key=${process.env.YT_API_KEY}`)
+  const YT_API = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${process.env.CHANNEL_ID}&key=${process.env.YT_API_KEY}`
+
+  const response = await fetch(YT_API)
   const data = await response.json()
-  console.log(data)
   const ytItems = data.items
   const viewCount = ytItems[0].statistics.viewCount
   const ytURL = {
