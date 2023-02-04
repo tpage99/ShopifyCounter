acba53c6c0d594b6279f275a69de32cf046a6530
Taylor Page <taylorpage@Taylors-MBP-67.lan>
2023-02-04 14:43:35 -0500

    seems like its not returning anything so just going to print and return the data

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -20,7 +20,7 @@ export const handler = async (event, context) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(ytViews),
+      body: JSON.stringify(data),
       headers: {
         'Content-Type': 'application/json'
       }
