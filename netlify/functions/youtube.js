37f930485d1d44f90c6ede3e7cdd29d9efafc7d2
Taylor Page <taylorpage@Taylors-MBP-67.lan>
2023-02-04 14:53:12 -0500

    problem is the api key is not being passed so some issue with environmental variable - resetting and trying again

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch'
 
-const channel_Id = process.env.CHANNEL_ID
-const Yt_Api_Key = process.env.YT_API_KEY
-
-const YT_API = `https://www.googleapis.com/youtube/v3/channels?part=statistics&fields=items(statistics(viewCount))&id=` + channel_Id + `&key=`+ Yt_Api_Key
-
 export const handler = async (event, context) => {
+
+  const channel_Id = process.env.CHANNEL_ID
+  const Yt_Api_Key = process.env.YT_API_KEY
+  const YT_API = `https://www.googleapis.com/youtube/v3/channels?part=statistics&fields=items(statistics(viewCount))&id=` + channel_Id + `&key=`+ Yt_Api_Key
+
   try {
     const response = await fetch(YT_API)
     const data = await response.json()
