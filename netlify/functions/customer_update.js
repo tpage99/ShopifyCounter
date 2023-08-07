175114f9ff5a94855c2dac0304d91a30d6bbdced
Taylor Page <taylor@thepagesmedia.com>
2023-08-07 17:02:57 -0400

    reworked to return msg identical to youtube

--- netlify/functions/customer_update.js
+++ netlify/functions/customer_update.js
@@ -1,35 +1,30 @@
 import fetch from 'node-fetch'
 
 export const handler = async (event, context) => {
-    if (event.httpMethod !== "POST") {
-        return { statusCode: 405, body: "Method Not Allowed" };
+  if (event.httpMethod !== "POST") {
+      return { statusCode: 405, body: "Method Not Allowed" };
+  }
+  const slackURL = process.env.SLACK_CUSTOMER_UPDATE_WEBHOOK_URL;
+  const incomingData = JSON.parse(event.body);
+
+  const slackMsg = `New customer update! ${incomingData.first_name} ${incomingData.last_name} just updated their info. Notes on this customer include: ${incomingData.note}.`
+
+  try {
+    const response = await fetch(slackURL)
+    
+    return {
+      statusCode: 200,
+      body: slackMsg,
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
-    const slackURL = process.env.SLACK_CUSTOMER_UPDATE_WEBHOOK_URL;
-    const incomingData = JSON.parse(event.body);
-
-    const slackMsg = `New customer update! ${incomingData.first_name} ${incomingData.last_name} just updated their info. Notes on this customer include: ${incomingData.note}.`
-
-    try {
-        const response = await fetch(`${slackURL}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(slackMsg)
-        });
-
-        const responseBody = await response.text();
-
-        return {
-            statusCode: 200,
-            body: `Data forwarded successfully: ${responseBody}`
-        };
-
-    } catch (error) {
-        console.error('Error forwarding data:', error);
-        return {
-            statusCode: 500,
-            body: 'Failed to forward data.'
-        };
-    }
+  }
+  catch (error) {
+    console.error('Error forwarding data:', error);
+    return {
+        statusCode: 500,
+        body: 'Failed to forward data.'
+    };
+  }
 };
