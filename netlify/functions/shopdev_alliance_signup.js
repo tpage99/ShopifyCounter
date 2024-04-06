bb01c6da8740a786c0bbe87b38737c3e31ecc36a
Taylor Page <taylor@thepagesmedia.com>
2024-04-06 15:01:01 -0400

    shopdev alliance signup and will see if this works

---
+++ netlify/functions/shopdev_alliance_signup.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch'
+
+export const handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+      return { statusCode: 405, body: "Method Not Allowed" };
+  }
+  const slackURL = process.env.SHOPDEV_ALLIANCE_SIGNUP_WEBHOOK_URL;
+  const incomingData = JSON.parse(event.body);
+  console.log(incomingData);
+  const name = incomingData.name;
+  const email = incomingData.email;
+  const slackMsg = {
+    "text": `New recruit! ${name} just requested to join the Alliance 🫡. They can be reached at: ${email}`
+  }
+
+  const response = await fetch(slackURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(slackMsg),
+  });
+  if (!response.ok) {
+    return { statusCode: response.status, body: 'Failed to send data' };
+  }
+  return { statusCode: 200, body: 'Data sent successfully' };
+};
