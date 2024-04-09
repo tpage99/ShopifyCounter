f0cef0f0a7f54f0ada419d04220ec28078735f64
Taylor Page <taylor@thepagesmedia.com>
2024-04-09 13:49:51 -0400

    final attempt for the day or will just revert back. found blog post so not using airtables recommended query structure

--- netlify/functions/airtable_update_signup.js
+++ netlify/functions/airtable_update_signup.js
@@ -1,37 +1,59 @@
 const Airtable = require('airtable');
 
-export const handler = async (event, context) => {
-  if (event.httpMethod !== "POST") {
-      return { statusCode: 405, body: "Method Not Allowed" };
-  }
-  const incomingData = JSON.parse(event.body);
-  console.log(incomingData);
-  const airtableToken = process.env.AIRTABLE_SIGNUP_TOKEN;
-  const name = incomingData.name.toString();
-  const email = incomingData.email.toString();
-  const signupTime = incomingData.created_at.toString();
-  const base = new Airtable({apiKey: airtableToken}).base('appvMCfxB0T6vV1TB');
+const handler = async (event) => {
+  // Configure Airtable base connection
+  const base = new Airtable({ apiKey: process.env.AIRTABLE_SIGNUP_TOKEN }).base('appvMCfxB0T6vV1TB')
 
-  console.log(`Updating table with: name: ${name} email: ${email} signed up date/time: ${signupTime}`);
-  console.log(`Airtable token: ${airtableToken}`)
-  
-  base('Imported table').create([
-    {
-      "fields": {
-        "name": `${name}`,
-        "email": `${email}`,
-        "signed up date/time": `${signupTime}`
+  // Configure table name
+  const table = base('Imported table')
+
+  try {
+    const { httpMethod } = event
+    const incomingData = JSON.parse(event.body);
+    console.log(incomingData);
+    const name = incomingData.name.toString();
+    const email = incomingData.email.toString();
+    const signupTime = incomingData.created_at.toString();
+
+    console.log(`Updating table with: name: ${name} email: ${email} signed up date/time: ${signupTime}`);
+
+    // Only allow POST
+    if (httpMethod !== "POST") {
+      return {
+        statusCode: 405,
+        body: JSON.stringify({ message: "Method Not Allowed" })
+      }
+    } else if (!name || !email || !signupTime) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "Bad Request" })
       }
     }
-  ], function(err, record) {
-    if (err) {
-      console.error(err);
-      return;
+
+    // Create record
+    // NOTE: Without `await`, this will not wait until data has been inserted into Airtable and return Status 202 due to async behaviour.
+    // As a result, we will receive 202 response code but our data are not inserted into our Airtable base.
+    await table
+      .create({
+        name,
+        email,
+        signupTime
+      })
+      .then((rec) => {
+        console.log("Successfully inserted into airtable")
+      })
+      .catch((err) => {
+        throw err
+      })
+    return {
+      statusCode: 202,
+      body: JSON.stringify({ message: "Accepted" })
     }
-    console.log(record.getId());
-  });
-};
+  } catch (error) {
+    return { statusCode: 500, body: "Oops! Something went wrong." }
+  }
+}
 
-
+module.exports = { handler }
 
 
