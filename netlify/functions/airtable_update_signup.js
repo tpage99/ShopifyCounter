75a340c3cf13a8b11ba225239dddd9bcb18c246f
Taylor Page <taylor@thepagesmedia.com>
2024-04-09 12:59:50 -0400

    console logging and pushing all to string values

--- netlify/functions/airtable_update_signup.js
+++ netlify/functions/airtable_update_signup.js
@@ -7,12 +7,13 @@ export const handler = async (event, context) => {
   const incomingData = JSON.parse(event.body);
   console.log(incomingData);
   const airtableToken = process.env.AIRTABLE_SIGNUP_TOKEN;
-  const name = incomingData.name;
-  const email = incomingData.email;
-  const signupTime = incomingData.created_at;
+  const name = incomingData.name.toString();
+  const email = incomingData.email.toString();
+  const signupTime = incomingData.created_at.toString();
   const base = new Airtable({apiKey: airtableToken}).base('appvMCfxB0T6vV1TB');
 
   console.log(`Updating table with: name: ${name} email: ${email} signed up date/time: ${signupTime}`);
+  console.log(`Airtable token: ${airtableToken}`)
   
   base('Imported table').create([
     {
